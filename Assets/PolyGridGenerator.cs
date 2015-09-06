8aa435eafbacad01e52384b77d59ec4b4d670eb8
Hjorthenify <Unknown>
2015-09-06 16:39:20 +0200

    Split the grid generation algorithm from Gizmo drawing. Now its not generated on every tick, but instead by the press of a button. Grid generation still needs some refactoring to split it into smaller pieces.

--- Assets/PolyGridGenerator.cs
+++ Assets/PolyGridGenerator.cs
@@ -4,58 +4,60 @@ using System.Collections.Generic;
 
 
 
+
 public class PolyGridGenerator : MonoBehaviour {
 
     // Use this for initialization
     Dictionary<Vector2, TileNode> PolyGrid = new Dictionary<Vector2, TileNode>();
-
-
-    void OnDrawGizmos()
+    public bool DebugDraw = false;
+    void Start()
     {
-        PolyGrid.Clear();
+        GenerateGrid();
+    }
+    
+    public void GenerateGrid()
+    {
+            System.Diagnostics.Stopwatch watch = System.Diagnostics.Stopwatch.StartNew();
+            PolyGrid.Clear();
 
-        //Gameobjects representing the two positions we are pathfidning between
-        GameObject target = GameObject.FindGameObjectWithTag("Player");
-        Vector2 goalPos = target.transform.position;GameObject.FindGameObjectWithTag("Player");
+            //Gameobjects representing the two positions we are pathfidning between
+            GameObject target = GameObject.FindGameObjectWithTag("Player");
+            Vector2 goalPos = target.transform.position; GameObject.FindGameObjectWithTag("Player");
 
-        GameObject monster = GameObject.FindGameObjectWithTag("Finish");
-        Vector2 monsterPos = monster.transform.position;
+            GameObject monster = GameObject.FindGameObjectWithTag("Finish");
+            Vector2 monsterPos = monster.transform.position;
 
-        //Workaround for the rays hitting the unit itself when exploring vertexes, which results in no valid paths
-        int targetLayer = target.layer;
-        target.layer = 2;
+            //Workaround for the rays hitting the unit itself when exploring vertexes, which results in no valid paths
+            int targetLayer = target.layer;
+            target.layer = 2;
 
-        int monsterLayer = monster.layer;
-        monster.layer = 2;
+            int monsterLayer = monster.layer;
+            monster.layer = 2;
 
 
 
-        Gizmos.color = Color.magenta;
-        //Was BoxCollider2D
-        Collider2D[] colliders = GameObject.FindObjectsOfType<Collider2D>();
-        Vector2[] corners = { new Vector2(-1, -1), new Vector2(1, 1), new Vector2(-1, 1), new Vector2(1, -1) };
-        //List<Vector2> Vertices = new List<Vector2>();
+            Collider2D[] colliders = GameObject.FindObjectsOfType<Collider2D>();
+            Vector2[] corners = { new Vector2(-1, -1), new Vector2(1, 1), new Vector2(-1, 1), new Vector2(1, -1) };
+            //List<Vector2> Vertices = new List<Vector2>();
 
-        foreach (Collider2D collider in colliders)
-        {
+            foreach (Collider2D collider in colliders)
+            {
 
-            //Objects with rigidbodies are skipped because they WILL move a lot
-            if (collider.attachedRigidbody != null)
-                continue;
+                //Objects with rigidbodies are skipped because they WILL move a lot. We also skip disabled objects as units shouldn't try to avoid these. 
+                if (collider.attachedRigidbody != null || !collider.isActiveAndEnabled)
+                    continue;
 
-            //Colors the colliders to visualize which ones it sees
-            collider.GetComponent<SpriteRenderer>().color = Color.cyan;
+                //Colors the colliders to visualize which ones it sees
+#if DEBUG
+                collider.GetComponent<SpriteRenderer>().color = Color.cyan;
+#endif
+               
+                //Finds each corner of each and every boxcollider
 
-            //BOUNDS IS A AABB CHECK! Which means it can't be used to figure out the points.. 
-            Bounds area = collider.bounds;
-            Gizmos.DrawSphere(area.center, 5);
-
-            //Finds each corner of each and every boxcollider
-            
                 BoxCollider2D boxCollider = collider as BoxCollider2D;
                 CircleCollider2D circleCollider;
                 PolygonCollider2D polyCollider;
-                
+
                 if ((boxCollider = collider as BoxCollider2D) != null)
                 {
                     foreach (Vector2 cornerOffset in corners)
@@ -65,94 +67,109 @@ public class PolyGridGenerator : MonoBehaviour {
                         Vector2 worldPos = collider.transform.TransformPoint(cornerPos);
                         //Adds each corner to a list of vertices which can be used to generate the map
                         PolyGrid.Add(worldPos, new TileNode(worldPos, new LineDebugger()));
-                        Gizmos.DrawSphere(worldPos, 5);
-
                     }
-                continue;
+                    continue;
                 }
-                else if((circleCollider = collider as CircleCollider2D) != null)
+                else if ((circleCollider = collider as CircleCollider2D) != null)
                 {
-                   
+
                     foreach (Vector2 offset in corners)
                     {
-                        
-                        Vector2 offsetPos = circleCollider.offset + (circleCollider.radius+1)*offset;
+
+                        Vector2 offsetPos = circleCollider.offset + (circleCollider.radius + 1) * offset;
                         Vector2 worldPos = collider.transform.TransformPoint(offsetPos);
                         PolyGrid.Add(worldPos, new TileNode(worldPos, new LineDebugger()));
-                        Gizmos.DrawSphere(worldPos, 5);
                     }
-                continue;
+                    continue;
                 }
-                else if((polyCollider = collider as PolygonCollider2D)!=null)
+                else if ((polyCollider = collider as PolygonCollider2D) != null)
                 {
                     //Loops over each vertex in the polygon collider and transform them to world space. 
-                    foreach(Vector2 point in polyCollider.points)
+                    foreach (Vector2 point in polyCollider.points)
                     {
                         //By how much should we multiply the vector with to incrase its lenght by 1?
                         float multiplyLenght = (point.magnitude + 1) / point.magnitude;
                         Vector2 worldPos = collider.transform.TransformPoint(point * multiplyLenght);
                         PolyGrid.Add(worldPos, new TileNode(worldPos, new LineDebugger()));
-                    Gizmos.DrawSphere(worldPos, 5);
+                        
                     }
                 }
-    
 
-        }
-        Debug.Log("Found " + colliders.Length + " colliders");
 
-        //Loops over each vertex and raycasts to the other vertices to check if they have LOS
-        int rayCounter = 0;
-        //Reuseable ray
-        RaycastHit2D[] ray = new RaycastHit2D[1];
+            }
+            Debug.Log("Found " + colliders.Length + " colliders");
 
-        //For testing purposes. Adds an end and start location: 
-        PolyGrid.Add(monsterPos, new TileNode(monsterPos, new LineDebugger()));
-        PolyGrid.Add(goalPos, new TileNode(goalPos, new LineDebugger()));
+            //Loops over each vertex and raycasts to the other vertices to check if they have LOS
+            int rayCounter = 0;
 
-        foreach (Vector2 vertex in PolyGrid.Keys)
-        {
-            foreach (Vector2 otherVertex in PolyGrid.Keys)
+            //Reuseable ray which saves memory allocations.
+            RaycastHit2D[] ray = new RaycastHit2D[1];
+
+            //For testing purposes. Adds an end and start location: 
+            PolyGrid.Add(monsterPos, new TileNode(monsterPos, new LineDebugger()));
+            PolyGrid.Add(goalPos, new TileNode(goalPos, new LineDebugger()));
+
+            //Checks for LOS from every node to every other node. 
+            foreach (Vector2 vertex in PolyGrid.Keys)
             {
-                //We dont want to check LOS for the current vertex
-                if (otherVertex == vertex)
-                    continue;
-                
-                //If we didn't hit any objects between the two vertices then we can add it as a valid path
-                if (Physics2D.LinecastNonAlloc(vertex, otherVertex, ray) == 0)
+                foreach (Vector2 otherVertex in PolyGrid.Keys)
                 {
-                    //RAINBOWS
-#if false
-                    Color[] Colors = {  Color.blue, Color.magenta, Color.green, Color.yellow, Color.red };
-                    Gizmos.color = Colors[Random.Range(0, 5)];
-#endif
-                  
-                    Gizmos.DrawLine(vertex, otherVertex);
+                    //We dont want to check LOS for the current vertex
+                    if (otherVertex == vertex)
+                        continue;
 
-                    //Adds the node as a possible vertex
-                    PolyGrid[vertex].AddEdge(otherVertex);
-                    ++rayCounter;
+                    //If we didn't hit any objects between the two vertices then we can add it as a valid path
+                    if (Physics2D.LinecastNonAlloc(vertex, otherVertex, ray) == 0)
+                    {
+                        //Adds the node as a possible vertex
+                        PolyGrid[vertex].AddEdge(otherVertex);
+                        ++rayCounter;
+                    }
                 }
+
+            }
+            Debug.Log("Casted " + rayCounter + " rays on " + PolyGrid.Count + " vertices");
+
+            Astar path = new AStarPolyGrid();
+
+            Vector2 prev = goalPos;
+            List<Vector2> route = path.GetPath(monsterPos, goalPos, PolyGrid);
+            Debug.Log("Route contained: " + route.Count);
+            foreach (Vector2 waypoint in route)
+            {
+                prev = waypoint;
             }
 
+            //Resets the layer back to the prior set before the raycast hackfix
+            target.layer = targetLayer;
+            monster.layer = monsterLayer;
+            watch.Stop();
+            Debug.Log("Generated Polygrid in " + watch.ElapsedMilliseconds + " miliseconds");
         }
-        Debug.Log("Casted " + rayCounter + " rays on " + PolyGrid.Count + " vertices");
-      
-        Astar path = new AStarPolyGrid();
-
-        Vector2 prev = goalPos;
-        List<Vector2> route = path.GetPath(monsterPos, goalPos, PolyGrid);
-        Debug.Log("Route contained: " + route.Count);
-        foreach(Vector2 waypoint in route)
+    
+        void OnDrawGizmos()
         {
-
-            Gizmos.color = Color.green;
-            Gizmos.DrawLine(prev, waypoint);
-            prev = waypoint;
+            if (!DebugDraw)
+                return;
+            Gizmos.color = Color.magenta;
+            
+            //Contains a list of the nodes already visited
+            HashSet<Vector2> visited = new HashSet<Vector2>();
+            foreach(KeyValuePair<Vector2, TileNode> Value in PolyGrid)
+            {
+                Gizmos.DrawSphere(Value.Key, 5);
+                visited.Add(Value.Key);
+                foreach(Vector2 neighbour in Value.Value)
+                {
+                    //if true, then we have already visited this node and drawn lines from it to its neighbours. There is therefore no reason to draw it again. 
+                    if(!visited.Contains(neighbour))
+                    {
+                      Gizmos.DrawLine(Value.Key, neighbour);
+                    }
+                }
+            }
         }
-        target.layer = targetLayer;
-        monster.layer = monsterLayer; 
-    }
+   
 
-        // Update is called once per frame 
 
 }
